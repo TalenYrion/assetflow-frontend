57a8da91ad2bc27dbdc6c27959cf47fadede37bc
talen <yrion18@example.com>
2026-06-26 06:09:38 -0700

    fix caching

--- src/context/query-context.tsx
+++ src/context/query-context.tsx
@@ -13,7 +13,9 @@ export default function QueryProvider({
       new QueryClient({
         defaultOptions: {
           queries: {
-            staleTime: 1000 * 60 * 5,
+            staleTime: 0, // Immediately marks data as stale so it always refetches
+            gcTime: 0,    // Disables caching in memory (Note: use `cacheTime: 0` instead if you are using TanStack Query v4)
+            refetchOnWindowFocus: false, // Optional: prevents extra backend hits when switching browser tabs
             retry: 1,
           },
         },
--- src/hooks/useLogout.ts
+++ src/hooks/useLogout.ts
@@ -1,19 +1,26 @@
 import { useAuth } from "@/context/auth-context"
 import { authApi } from "@/lib/api/auth"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
 import { toast } from "sonner"
 
 export function useSignOut () {
 	const {refreshUser} = useAuth();
 	const router = useRouter()
-
+	const queryClient = useQueryClient()
 	return useMutation({
 		mutationFn: authApi.logout,
 		onSuccess:async () => {
 toast.success('logged out Successfully')
 await refreshUser()
-
+      queryClient.invalidateQueries({ queryKey: ['seller'] });
+      queryClient.invalidateQueries({
+        queryKey: ['feed-assets'],
+      });
+      queryClient.invalidateQueries({ queryKey: ['buyer'] });
+      queryClient.invalidateQueries({
+        queryKey: ['file-type'],
+      });
 		},
 	onError: (error: Error) => {
       toast.error(error.message || 'Failed to sign out. Please try again.');
