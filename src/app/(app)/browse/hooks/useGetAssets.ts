0528877d92ff9f85c9c782cce4c629e203666514
talen <yrion18@example.com>
2026-06-26 04:46:10 -0700

    fix browser

--- src/app/(app)/browse/hooks/useGetAssets.ts
+++ src/app/(app)/browse/hooks/useGetAssets.ts
@@ -8,7 +8,12 @@ export function useGetAssets(input: BrowserQuery) {
   const query = useQuery({
     queryKey: ['feed-assets', input],
     queryFn: () => browserApi.getAllAssets(input),
-    placeholderData: (previousData) => previousData,
+    
+    // Disable caching:
+    gcTime: 0, // Note: If you are using React Query v4 or older, change this to `cacheTime: 0`
+    staleTime: 0, 
+    
+     placeholderData: (previousData) => previousData, // Optional: Remove this if you don't want old data lingering on screen during a refetch
   });
 
   useEffect(() => {
